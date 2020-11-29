1b0972f2e6c6aae21398329371b9d00e50261c6b
kirtanlab <kirtaprajapati0804@gmail.com>
2020-11-29 22:47:43 +0530

    initi

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 // just for commiting changes
+//just for  commiting changes 
 // Load all event listeners
 loadEventListeners();
 
